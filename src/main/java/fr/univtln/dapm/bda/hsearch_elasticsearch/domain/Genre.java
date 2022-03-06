63d9d616fe6d59b98174e86fb3558c4c219b4bcc
kevin534 <60352713+kevin534@users.noreply.github.com>
2022-03-06 23:24:03 +0100

    My Commit 1

--- src/main/java/fr/univtln/dapm/bda/hsearch_elasticsearch/domain/Genre.java
+++ src/main/java/fr/univtln/dapm/bda/hsearch_elasticsearch/domain/Genre.java
@@ -1,19 +1,14 @@
 package fr.univtln.dapm.bda.hsearch_elasticsearch.domain;
 
 import java.io.Serializable;
+import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 import org.hibernate.search.engine.backend.types.Projectable;
 import org.hibernate.search.engine.backend.types.TermVector;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
-import org.hibernate.search.mapper.pojo.mapping.definition.annotation.IndexedEmbedded;
 
 /**
  * Entité indexable représentant un genre.
@@ -21,6 +16,7 @@ import org.hibernate.search.mapper.pojo.mapping.definition.annotation.IndexedEmb
  * @author vincent
  *
  */
+
 @Entity
 @Indexed
 public class Genre implements Serializable {
@@ -33,20 +29,20 @@ public class Genre implements Serializable {
     private String type;
     @FullTextField(analyzer = "m1_did_analyzer", projectable = Projectable.YES, termVector = TermVector.WITH_POSITIONS_OFFSETS)
     private String description;
-    @Lob
+
+    @OneToMany (mappedBy = "genre")
+    private List<Book> listBook;
+
 
     public String getType() {
         return type;
     }
-
     public void setType(String type) {
         this.type = type;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
